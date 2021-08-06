dc029c6cc56bcf114bb2ee755f76ca211504ee0a
pervysage <zulkerb9b@gmail.com>
2021-08-07 03:56:59 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
 import About from "./About/About";
+// import companyLogo from "./Zulker_Logo.png";
+import "./styles.css"
 const App = () => {
   return (
     <>
+      {/* <div className="ZulkerLogo"> */}
+      {/* <img src={companyLogo} alt="BigCo Inc. logo" /></div> */}
       <Header />
       <About />
     </>
